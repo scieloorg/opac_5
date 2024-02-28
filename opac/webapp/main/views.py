93bbec5b28f58a7338f369534129da5cb8525c68
Roberta Takenaka <roberta.takenaka@scielo.org>
2024-02-28 18:35:34 -0300

    Refatora o meio de obter os artigos, as seções e os artigos filtrados

--- opac/webapp/main/views.py
+++ opac/webapp/main/views.py
@@ -949,18 +949,18 @@ def issue_toc(url_seg, url_seg_issue):
             len(journal.study_areas or []) >= current_app.config["FILTER_SECTION_ENABLE_FOR_MIN_STUDY_AREAS"]
         )
 
-    # obtém os documentos
+    # obtém todos os documentos
     articles = controllers.get_articles_by_iid(issue.iid, is_public=True)
-    if articles:
-        # obtém TODAS as seções dos documentos deste sumário
-        sections = sorted({a.section.upper() for a in articles if a.section})
-    else:
-        # obtém as seções dos documentos deste sumário
-        sections = []
 
-    if current_app.config["FILTER_SECTION_ENABLE"] and section_filter != "":
-        # obtém somente os documentos da seção selecionada
-        articles = [a for a in articles if a.section.upper() == section_filter]
+    # obtém todas as seções
+    sections = sorted({
+        s.upper()
+        for s in articles.item_frequencies("section", normalize=True).keys()
+    })
+
+    # obtém os documentos da seção selecionada
+    if section_filter:
+        articles = articles.filter(section__iexact=section_filter)
 
     # obtém PDF e TEXT de cada documento
     has_math_content = False
